7cd4064a4bedb37c49dcf17d58702f6bfcad5d44
Anys Luthfia Ulfa <anys.luthfia@gmail.com>
2013-10-30 15:09:22 +0700

    Latihan Modul 4
    110533430651_Anys Luthfia Ulfa

---
+++ Tugas Modul 4/set_session.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD html 4.0 Transitional//EN".
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Set Session</title>
+</head>
+
+<body>
+
+<?php 
+
+//Inisialisasi data session
+session_start();
+
+//Set session jika belum ada
+if (!isset($_SESSION['test'])) {
+	$_SESSION['test'] = 'value';
+	
+} else {
+	echo 'Session di-set <br />';
+	//Mencetak nilai session test
+	echo 'Nilai: ' . $_SESSION['test'] . '<br />';
+	
+	//Mencetak semua elemen session
+	print_r($SESSION);
+}
+
+?>
+<p>
+Tekan refresh (F5);
+
+</body>
+</html>
---
+++ Tugas Modul 4/test_cookie.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD html 4.0 Transitional//EN".
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Cek Dukungan Cookie</title>
+</head>
+
+<body>
+
+<?php
+
+if (isset($_GET['q']) && $_GET['q'] == 1) {
+
+	if (isset($_COOKIE['test'])) {
+		echo 'support';
+	} else {
+		echo 'tidak support';
+	}
+	
+} else {
+	setcookie('test', 'value');
+	$self = $_SERVER['PHP_SELF'];
+	
+	//Redireksi ke current script
+	header('Location: ' . $self . '?q=1');
+	exit;
+}
+
+?>
+
+<p>
+Tekan Refresh 9F5);
+</body>
+</html>
